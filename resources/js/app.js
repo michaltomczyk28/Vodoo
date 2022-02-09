6afba6a2ddf79eeecff131a38906111853825134
Michal Tomczyk <michal.tomczyk@dwgdesign.pl>
2022-02-09 23:39:42 +0100

    Install and configure Vue 3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-require('./bootstrap');
+import * as Vue from 'vue'
+import App from './views/App.vue'
+
+const app = Vue.createApp(App)
+app.mount('#app')
+
---
+++ resources/js/views/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        Main app
+    </div>
+</template>
+
+<script>
+    export default{
+
+    }
+</script>
